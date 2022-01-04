cc30200f1d6623b4ad222944242ed51687370beb
james-muriithi <muriithijames556@gmail.com>
2022-01-04 17:12:33 +0300

    added quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BannerComponent } from './banner/banner.component';
 import { QuotesSectionComponent } from './quotes-section/quotes-section.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     BannerComponent,
-    QuotesSectionComponent
+    QuotesSectionComponent,
+    QuoteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from '../shared/quote.model';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.scss']
+})
+export class QuoteComponent implements OnInit {
+
+  @Input() quote : Quote| null = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
